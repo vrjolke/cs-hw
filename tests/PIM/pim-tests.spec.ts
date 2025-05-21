c4b1e602fa89900eb562dd6c6e87e8d4a25ae4a0
bence <bence.arvai@outlook.com>
2025-05-21 03:01:32 +0200

    add upload profile picture test

--- tests/PIM/pim-tests.spec.ts
+++ tests/PIM/pim-tests.spec.ts
@@ -4,6 +4,8 @@ import { createEmployee, deleteEmployee } from '../utils/employees';
 
 test.describe('PIM tests', () => {
 
+    let createdEmployeeIds: string[] = [];
+
     test.beforeEach(async ({ page }) => {
         await page.goto('/');
         await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
@@ -12,19 +14,35 @@ test.describe('PIM tests', () => {
         await page.getByRole('link', { name: 'PIM' }).click();
     });
 
+    test.afterEach(async ({ page }) => {
+        if (!createdEmployeeIds.length) return;
+
+        for (const id of createdEmployeeIds) {
+            await deleteEmployee(page, id);
+        }
+
+        createdEmployeeIds = [];
+    });
+
     test('create users', async ({ page }) => {
-        await page.getByRole('link', { name: 'Add Employee' }).click();
-        await expect(page.getByRole('textbox', { name: 'First Name' })).toBeVisible();
         const createdEmployeeIds: string[] = [];
 
         for (let employee of employees) {
             const employeeId = await createEmployee(page, employee);
             createdEmployeeIds.push(employeeId);
         }
+    });
 
-        //cleanup
-        for (let employeeId of createdEmployeeIds) {
-            await deleteEmployee(page, employeeId);
-        }
+    test('upload profile picture', async ({ page }) => {
+        const employeeId = await createEmployee(page, employees[0]);
+        createdEmployeeIds.push(employeeId);
+
+        await page.locator('.employee-image').click();
+        const fileChooserPromise = page.waitForEvent('filechooser');
+        await page.locator('.bi-plus').click();
+        const fileChooser = await fileChooserPromise;
+        await fileChooser.setFiles(require('path').resolve(__dirname, './fixtures/profile_picture.png'));
+        await page.getByRole('button', { name: 'Save' }).click();
+        await expect(page.getByText('Successfully Updated')).toBeVisible();
     });
 });
--- tests/utils/employees.ts
+++ tests/utils/employees.ts
@@ -7,6 +7,8 @@ type Employee = {
 };
 
 export async function createEmployee(page, employee: Employee): Promise<string> {
+    await page.getByRole('link', { name: 'Add Employee' }).click();
+    await expect(page.getByRole('textbox', { name: 'First Name' })).toBeVisible();
     await page.getByRole('textbox', { name: 'First Name' }).fill(employee.firstName);
     await page.getByRole('textbox', { name: 'Middle Name' }).fill(employee.middleName || '');
     await page.getByRole('textbox', { name: 'Last Name' }).fill(employee.lastName);
@@ -15,7 +17,6 @@ export async function createEmployee(page, employee: Employee): Promise<string> 
 
     await page.getByRole('button', { name: 'Save' }).click();
     await expect(page.getByRole('heading', { name: 'Personal Details' })).toBeVisible({ timeout: 10_000 });
-    await page.getByRole('link', { name: 'Add Employee' }).click();
 
     return employeeId;
 }
@@ -24,7 +25,11 @@ export async function deleteEmployee(page, employeeId: string) {
     await page.getByRole('link', { name: 'Employee List' }).click();
     await page.locator('div:has-text("Employee Id") + div input').fill(employeeId);
     await page.getByRole('button', { name: 'Search' }).click();
-    const deleteButton = page.getByRole('row').filter({ hasText: employeeId }).locator('button:has(.bi-trash)');
+    const employeeRow = await page.getByRole('row').filter({ hasText: employeeId });
+
+    await expect(employeeRow).toBeVisible();
+
+    const deleteButton = await page.getByRole('row').filter({ hasText: employeeId }).locator('button:has(.bi-trash)');
     await deleteButton.click();
     await page.locator('button:has-text("Yes, Delete")').click();
 }
