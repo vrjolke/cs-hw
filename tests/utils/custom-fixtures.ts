ab800875e66e420c0772b3514c3ae47bcdb063e0
bence <bence.arvai@outlook.com>
2025-05-28 20:22:31 +0200

    renaming files

--- tests/utils/custom-fixtures.ts
+++ tests/utils/custom-fixtures.ts
@@ -1,11 +1,13 @@
 import { test as baseTest } from '@playwright/test';
-import { LoginPage } from './pages/login-page';
+import { LoginPage } from '../pages/login-page';
+import { PimPage } from '../pages/pim-page';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 type Fixtures = {
   loginPage: LoginPage;
+  pimPage: PimPage;
   username: string;
   password: string;
 };
@@ -13,6 +15,9 @@ type Fixtures = {
 export const test = baseTest.extend<Fixtures>({
   loginPage: async ({ page }, use) => {
     await use(new LoginPage(page));
+  },
+  pimPage: async ({ page }, use) => {
+    await use(new PimPage(page));
   },
   username: async ({}, use) => {
     await use(process.env.ADMIN_USERNAME!);
